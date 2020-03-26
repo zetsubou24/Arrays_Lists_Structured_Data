90169dcb659f5fbdbf191959e8be427787f1f280
zetsubou24 <alphabetaomega4@gmail.com>
2020-03-26 16:34:01 +0530

    Fix condition

--- Week1/WordPlay.java
+++ Week1/WordPlay.java
@@ -20,9 +20,9 @@ public class WordPlay {
     public String emphasize(String phrase, char ch){
         StringBuilder sb = new StringBuilder();
         for(int i = 0; i < phrase.length(); i++){
-            if(phrase.charAt(i) == ch) {
-                if (i % 2 == 0) sb.append('+');
-                else sb.append('*');
+            if(Character.toLowerCase(phrase.charAt(i)) == ch) {
+                if (i % 2 == 0) sb.append('*');
+                else sb.append('+');
             }
             else sb.append(phrase.charAt(i));
         }
